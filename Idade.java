5327b3e32c327dbcd65156d96282fde17a1fbf62
Lailson96 <lailsonrd@gmail.com>
2024-08-14 22:22:56 -0300

    20241408

---
+++ Idade.java
@@ -0,0 +1,30 @@
+public class Idade {
+    //atributos
+        private int dia;
+        private int mes;
+        private int ano;
+    //metodo contrutor
+        public idade(int dd, int mm, int aaaa){
+            this.dia=dd;
+            this.mes=mm;
+            this.ano=aaaa;
+        }
+        public idade(){
+            this.dia=1;
+            this.dia=2;
+            this.dia=3;
+        }
+        //método de acesso * modificar o valor
+        public void setNum1(int dia){
+            this.dia=dia;
+        }
+        //método de acesso * obter o valor
+        public int getNum1(){
+            return(this.dia);
+        //Métodos
+        public int anos(){
+            
+        }
+    
+        }
+    
